6109e907ff1d654bded6b499e5bf798c2fa759e3
Yuchen Ni <niyuchen0101@163.com>
2024-03-04 09:40:40 +1100

    initial commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# the basic code sturcture of express.js
+* app.js -> for express middleware registration
+* server.js -> for server configuration
+* xxxRoutes.js -> for routes definition
+    * param middleware for centralized process of param
+* xxxController.js -> for middleware route handlers
---
+++ routes/tourRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const tourController = require('./../controllers/tourController');
+
+const router = express.Router(); // create the route
+
+router.param('id', tourController.checkID); // param middleware, applies when one parameter matches
+
+router
+  .route('/')  // relative to the root route in app.js
+  .get(tourController.getAllTours)
+  .post(tourController.checkBody, tourController.createTour);
+
+router
+  .route('/:id')  // relative to the root route in app.js
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
+
+module.exports = router;
